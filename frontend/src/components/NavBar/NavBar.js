36a7bab0cdc4d74b67e6c81ef1c55e8db525313e
thaddydore <thaddydore@gmail.com>
2021-04-09 00:23:17 +0100

    remove dropdown on navBar

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -4,36 +4,14 @@ import { FcSoundRecordingCopyright } from 'react-icons/fc';
 
 import './navBar.scss';
 
+
 const Navbar = () => {
   return (
     <nav className='navbar'>
-      <div className='navbar__logo-container'>
-        <div className='navbar--avatar'>
-          <Nav.Link href='/'>
-            <FcSoundRecordingCopyright size='2rem' />
-          </Nav.Link>
-        </div>
-      </div>
-
-      <Dropdown>
-        <div className='mr-2 navbar__notification'>
-          <span className='navbar__notification--notification-icon'>
-            <Nav.Link href="/logs">Activity Logs</Nav.Link>
-          </span>
-        </div>
-        <Dropdown.Toggle
-          variant='outline'
-          id='dropdown-basic'
-          className='d-flex'
-        >
-        </Dropdown.Toggle>
-
-        <Dropdown.Menu>
-          <Dropdown.Item href='#/action-1'>Action</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-
-      {/* </div> */}
+      <Nav.Link href='/'>
+        <FcSoundRecordingCopyright size='3rem' />
+      </Nav.Link>
+      <Nav.Link href="/logs">Activity Logs</Nav.Link>
     </nav>
   )
 }
